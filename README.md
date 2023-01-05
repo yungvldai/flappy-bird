32ad528fe2a4eca71a1a7676f09c77868205b0b7
Vlad Ivanov <vladivanov.dev@gmail.com>
2023-01-05 18:54:35 +0100

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# QR game - Flappy bird
+
+QR coded game. No internet required.
+
+## Usage
+```console
+npm i && npm run compile
+```
---
+++ scripts/compile.js
@@ -0,0 +1,33 @@
+import { minify } from 'html-minifier';
+import { readFileSync, writeFileSync } from 'fs';
+import { resolve, join } from 'path';
+import { toFile } from 'qrcode';
+import { execSync } from 'child_process';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+const input = readFileSync(resolve(__dirname, '../index.html'), { encoding: 'utf-8' });
+
+const minified = minify(input, {
+  collapseWhitespace: true,
+  minifyCSS: true,
+  minifyJS: {
+    mangle: {
+      toplevel: true
+    }
+  }
+});
+
+const outDir = resolve(__dirname, '../output');
+
+execSync(`mkdir -p ${outDir}`);
+writeFileSync(join(outDir, 'index.min.html'), minified);
+
+const b64 = Buffer.from(minified).toString('base64');
+const toEncode = `data:text/html;base64,${b64}`;
+
+console.log('Output size is', toEncode.length, 'bytes');
+
+writeFileSync(join(outDir, 'b64.txt'), toEncode);
+toFile(join(outDir, 'image.png'), toEncode);
